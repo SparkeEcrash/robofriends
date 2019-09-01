cd06bd2e7a1c3ff73f2868ee7197b51ab5d00be0
SparkeEcrash <jpark121989@gmail.com>
2019-08-31 20:25:09 -0700

    Updates

---
+++ precache-manifest.0ff15b2dc6e0c5aef06e2ba0bf66e80f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c68b8410289263769f9ad7e04fcd93",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2cf60acd4b09cf8cd3ca",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "80008d5c8647efe64eea",
+    "url": "/robofriends/static/css/main.81a37fed.chunk.css"
+  },
+  {
+    "revision": "2cf60acd4b09cf8cd3ca",
+    "url": "/robofriends/static/js/2.924203e4.chunk.js"
+  },
+  {
+    "revision": "80008d5c8647efe64eea",
+    "url": "/robofriends/static/js/main.dd62df0d.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
